0d30378bd4fb4702dbac370aa6d03fe678a413c7
Joaquin Guardado <owner@Owners-MacBook-Pro-5.local>
2016-02-07 16:12:10 -0700

    Added grunt components

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'css/style.css':'sass/style.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('dist', ['sass']);
+	grunt.registerTask('default', ['watch']);
+};
